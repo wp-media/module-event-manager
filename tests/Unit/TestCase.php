4f6da3db1cc42b1b290aef43c6b938fe80f63f30
Rémy Perona <remperona@gmail.com>
2020-01-07 16:19:19 -0500

    remove unnecessary method

--- tests/Unit/TestCase.php
+++ tests/Unit/TestCase.php
@@ -30,38 +30,4 @@ abstract class TestCase extends PHPUnitTestCase {
         Monkey\tearDown();
         parent::tearDown();
     }
-
-    /**
-     * Mock common WP functions.
-     *
-     * @author Grégory Viguier
-     * @access protected
-     */
-    protected function mockCommonWpFunctions() {
-        Monkey\Functions\stubs(
-            [
-                '__',
-                'esc_attr__',
-                'esc_html__',
-                '_x',
-                'esc_attr_x',
-                'esc_html_x',
-                '_n',
-                '_nx',
-                'esc_attr',
-                'esc_html',
-                'esc_textarea',
-                'esc_url',
-            ]
-        );
-        $functions = [
-            '_e',
-            'esc_attr_e',
-            'esc_html_e',
-            '_ex',
-        ];
-        foreach ( $functions as $function ) {
-            Monkey\Functions\when( $function )->echoArg();
-        }
-    }
 }
