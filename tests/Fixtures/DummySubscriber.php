c14e96df11cb3544a3dbef3b1ed8f22bad0bd9ed
Rémy Perona <remperona@gmail.com>
2025-11-21 16:52:02 -0500

    update fixture setup

--- tests/Fixtures/DummySubscriber.php
+++ tests/Fixtures/DummySubscriber.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace WPMedia\EventManager\Tests\Fixtures;
 
@@ -8,15 +9,20 @@ use WPMedia\EventManager\SubscriberInterface;
  * Dummy subscriber for tests
  */
 class DummySubscriber implements SubscriberInterface {
-    public static function getSubscribedEvents() {
-        return [
-            'foo' => 'bar',
-            'foofoo' => [ 'barbar', 20 ],
-            'foobar' => [ 'foobarbar', 50, 3 ],
-            'barfoo' => [
-                [ 'filter_this' ],
-                [ 'filter_that', 10, 2 ],
-            ],
-        ];
-    }
+	/**
+	 * Get the events to which the subscriber wants to listen
+	 *
+	 * @return array<string, string|array<string, mixed>|array<array<string, mixed>>>
+	 */
+	public static function get_subscribed_events(): array {
+		return [
+			'foo'    => 'bar',
+			'foofoo' => [ 'barbar', 20 ],
+			'foobar' => [ 'foobarbar', 50, 3 ],
+			'barfoo' => [
+				[ 'filter_this' ],
+				[ 'filter_that', 10, 2 ],
+			],
+		];
+	}
 }
