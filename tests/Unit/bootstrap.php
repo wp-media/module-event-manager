f526c3faed558f8fc554bdc772252a100072216d
Rémy Perona <remperona@gmail.com>
2025-11-21 16:51:48 -0500

    update unit tests configuration

--- tests/Unit/bootstrap.php
+++ tests/Unit/bootstrap.php
@@ -1,13 +1,7 @@
 <?php
-/**
- * Bootstraps the plugin's Unit Tests
- *
- * @package WPMedia\EventManager\Tests\Unit
- */
 
 namespace WPMedia\EventManager\Tests\Unit;
 
-use function WPMedia\EventManager\Tests\init_test_suite;
-
-require_once dirname( dirname( __FILE__ ) ) . '/bootstrap-functions.php';
-init_test_suite( 'Unit' );
+define( 'WPM_EVENT_MANAGER_PLUGIN_ROOT', dirname( dirname( __DIR__ ) ) . DIRECTORY_SEPARATOR );
+define( 'WPM_EVENT_MANAGER_TESTS_FIXTURES_DIR', dirname( __DIR__ ) . '/Fixtures' );
+define( 'WPM_EVENT_MANAGER_TESTS_DIR', __DIR__ );
